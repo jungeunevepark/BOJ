dfd2735d86795e568e3a9e079144ed600d2b0f30
evejj <101235186+jungeunevepark@users.noreply.github.com>
2023-02-06 21:29:55 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 268 ms, Memory: 42048 KB -BaekjoonHub

---
+++ 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int N;
+	static StringBuilder sb;
+	static int count;
+	public static void main(String[] args) throws Exception{
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		N = Integer.parseInt(bf.readLine());
+		
+		hanoi(N, 1, 2, 3);
+		System.out.println(count);
+		System.out.println(sb.toString());
+	}
+	static void hanoi(int n, int from, int mid, int to) {
+		if(n ==0) {
+			return;
+		}
+		hanoi(n-1, from, to, mid);
+		sb.append(from).append(" ").append(to).append("\n"); count++;
+		hanoi(n-1, mid, from, to);
+	}
+}
