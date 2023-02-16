b4ebf988481737b3f4950b51b1274e309514c490
evejj <101235186+jungeunevepark@users.noreply.github.com>
2023-02-16 20:44:11 +0900

    [Gold IV] Title: 배열 돌리기 4, Time: 348 ms, Memory: 27316 KB -BaekjoonHub

--- 백준/Gold/17406. 배열 돌리기 4/배열 돌리기 4.java
+++ 백준/Gold/17406. 배열 돌리기 4/배열 돌리기 4.java
@@ -1,101 +1,105 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-class Node{
-	public int x;
-	public int y;
-	public int d;
-	Node(int x, int y, int d){
-		this.x = x; this.y = y; this.d = d;
-	}
-}
 public class Main {
-	static int N;
-	static int M;
-	static int K;
-	static int[][] A;
-	static int[][] rotate;
-	static int result;
+	static int[] di = { 1, 0, -1, 0 };
+	static int[] dj = { 0, 1, 0, -1 };
+	static int[][] map;
+	static int n;
+	static int m;
+	static int k;
+	static int[][] ways;
+	static int[] res;
+	static boolean[] v;
+	static int MIN;
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
-        
-        N= Integer.parseInt(st.nextToken());
-        M= Integer.parseInt(st.nextToken());
-        K= Integer.parseInt(st.nextToken());
-        
-        A = new int[N][M];
-        for(int i=0; i<N; i++) {
-        	st = new StringTokenizer(bf.readLine(), " ");
-        	for(int j=0; j<M; j++) {
-        		A[i][j] = Integer.parseInt(st.nextToken());
-        	}
-        }
-        rotate = new int[K][];
-        for(int i=0; i<K; i++) {
-        	st = new StringTokenizer(bf.readLine(), " ");
-        	int r = Integer.parseInt(st.nextToken());
-        	int c = Integer.parseInt(st.nextToken());
-        	int s = Integer.parseInt(st.nextToken());
-        	rotate[i] = new int[] {r-s-1, c-s-1, 2*s+1, 0};
-        }
-        result = Integer.MAX_VALUE;
-        dfs(0);
-        sb.append(result).append("\n");
-        System.out.println(sb.toString());
-    }
-    static void dfs(int count) {
-    	if(count == K) {
-    		int c =0;
-    		int[][] check = new int[N][M];
-    		for(int i=0; i<N; i++)
-    			for(int j=0; j<M; j++)
-    				check[i][j] = A[i][j];
-    		while(c<K) {
-    			for(int i=0; i<K; i++)
-    				if(rotate[i][3] == c)
-    					rotation(rotate[i][0], rotate[i][1], rotate[i][2], check);
-    			c++;
-    		}
-    		for(int i=0; i<N; i++) {
-    			int sum = 0;
-    			for(int j=0; j<M; j++)
-    				sum += check[i][j];
-    			if(result > sum) result = sum;
-    		}
-    		return;
-    	}
-    	
-    	for(int i=0; i<K; i++) {
-    		if(rotate[i][3] != 0) continue;
-    		rotate[i][3] = count;
-    		dfs(count+1);
-    		rotate[i][3] = 0;
-    	}
-    }
-    
-    static void rotation(int r, int c, int s, int[][] A) {
-    	int now = 0;
-    	int[][] B = new int[s][s];
-    	while(now < s/2) {
-    		for(int j=now; j<s-now-1; j++) 
-    			B[now][j+1] = A[now+r][j+c];
+	static void cal(int[][] arr) {
+		for (int i = 0; i < n; i++) {
+			int cnt = 0;
+			for (int j = 0; j < m; j++) {
+				cnt += arr[i][j];
+			}
+			MIN = Math.min(MIN, cnt);
+		}
+	}
 
-    		for(int i=now; i<s-now-1; i++)
-    			B[i+1][s-now-1] = A[i+r][s-now+c-1];
-    		
-    		for(int j=s-now-1; j>now; j--)
-    			B[s-now-1][j-1] = A[s-now-1+r][j+c];
-    		
-    		for(int i=s-now-1; i>now; i--)
-    			B[i-1][now] = A[i+r][now+c];
-    		now++;
-    	}
-    	if(s % 2 == 1) B[s / 2][s/2] = A[s / 2+r][s/2+c];
-    	for(int i=0; i<s; i++)
-    		for(int j=0; j<s; j++)
-    			A[i+r][j+c] = B[i][j];
-    }
+	static void rot(int[][] arr, int s_x, int s_y, int val) {
+		if (val == 1)
+			return;
+		int tmp = arr[s_x][s_y];
+		int x = s_x;
+		int y = s_y;
+		int dir = 0;
+		while (dir < 4) {
+			int nx = x + di[dir];
+			int ny = y + dj[dir];
+			if (nx < s_x || s_x + val <= nx || n <= nx | ny < s_y || s_y + val <= ny || m <= ny) {
+				dir++;
+				continue;
+			}
+			arr[x][y] = arr[nx][ny];
+			x = nx;
+			y = ny;
+
+		}
+		arr[s_x][s_y + 1] = tmp;
+		rot(arr, s_x + 1, s_y + 1, val - 2);
+	}
+
+	static void perm(int cnt) {
+		if (cnt == k) {
+			int[][] tmp = new int[n][m];
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < m; j++) {
+					tmp[i][j] = map[i][j];
+				}
+			}
+			for (int i = 0; i < k; i++) {
+				rot(tmp, ways[res[i]][0], ways[res[i]][1], ways[res[i]][2]);
+//				for (int[] t : tmp)
+//					System.out.println(Arrays.toString(t));
+//				System.out.println();
+			}
+			cal(tmp);
+			return;
+		}
+		for (int i = 0; i < k; i++) {
+			if (v[i])
+				continue;
+			v[i] = true;
+			res[cnt] = i;
+			perm(cnt + 1);
+			v[i] = false;
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+		map = new int[n][m];
+		ways = new int[k][3];
+		res = new int[k];
+		v = new boolean[k];
+		MIN = Integer.MAX_VALUE;
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < m; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		for (int i = 0; i < k; i++) {
+			st = new StringTokenizer(br.readLine());
+			int r = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			int s = Integer.parseInt(st.nextToken());
+			ways[i] = new int[] { r - s - 1, c - s - 1, 2 * s + 1 };
+		}
+		perm(0);
+		System.out.println(MIN);
+	}
 }
