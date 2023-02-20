194d181e671344d2d88ccb8480c3bafe82b9ff4e
evejj <101235186+jungeunevepark@users.noreply.github.com>
2023-02-20 12:16:01 +0900

    [Gold V] Title: 인구 이동, Time: 380 ms, Memory: 115496 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -18,17 +18,24 @@ class Node {
 }
 
 public class Main {
+	static int N, L, R;
+	static int[][] A;
+	static boolean[][] visited;
+	static int[] di = { 0, 0, -1, 1 };
+	static int[] dj = { 1, -1, 0, 0 };
+	static int sum, cnt;
+	static ArrayDeque<int[]> move;
 	public static void main(String[] args) throws Exception {
 		// N, L, R 입력 받음
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 	    String[] line = bf.readLine().split(" ");
-	    int N = Integer.valueOf(line[0]);
-	    int L = Integer.valueOf(line[1]);
-	    int R = Integer.valueOf(line[2]);
+	    N = Integer.valueOf(line[0]);
+	    L = Integer.valueOf(line[1]);
+	    R = Integer.valueOf(line[2]);
 
 		// 나라 배열 A 선언
 		// 각 나라의 인구수를 값으로 하는 배열
-		int[][] A = new int[N][N];
+		A = new int[N][N];
 
 		// A 값 할당: i=행, j=열
 		for (int i = 0; i < N; i++) {
@@ -39,90 +46,93 @@ public class Main {
 
 		int result = 0; // 인구 이동 횟수
 		while (true) {
-			if (migration(A, L, R) == 0)
+			if (migration() == false)
 				break;
 			result++;
 		}
 		System.out.println(result);
 		bf.close();
 	}
+	static void bfs(int i, int j) {
+		// 그룹을 찾는데 쓰는 queue
+		ArrayDeque<int[]> q = new ArrayDeque<>();
+		q.add(new int[] {i, j});
+		
+		move.add(new int[] {i, j});
+		visited[i][j] = true;
+		boolean flag = false; // return 값 정의
+		
+		// 더 이상 그룹에 들어올 나라가 없는 경우 while문 종료
+		while (!q.isEmpty()) {
+			int[] p = q.poll();
 
-	static int migration(int[][] A, int L, int R) {
+			for (int d = 0; d < 4; d++) {
+				// 상, 하, 좌, 우 조사
+				int ni = p[0] + di[d];
+				int nj = p[1] + dj[d];
+
+				// 주어진 땅 크기를 넘어가거나 이미 확인을 마친 나라의 경우 넘어감
+				if (ni < 0 || nj < 0 || ni >= N || nj >= N || visited[ni][nj])
+					continue;
+
+				// 인구 차이를 조사하여 국경선을 열지 말지 결정
+				if (Math.abs(A[p[0]][p[1]] - A[ni][nj]) >= L && Math.abs(A[ni][nj] - A[p[0]][p[1]]) <= R) {
+					visited[ni][nj] = true;
+					sum += A[ni][nj];
+					cnt++;
+
+					// 방문 표시를 하고, 다음 조사할 나라로 queue에 넣어줌
+					q.add(new int[] {ni, nj});
+					move.add(new int[] {ni, nj});
+				}
+			}
+		}
+	}
+	static void dfs(int i, int j) {
+		visited[i][j] = true;
+		move.offer(new int[] {i, j});
+		for (int d = 0; d < 4; d++) {
+			// 상, 하, 좌, 우 조사
+			int ni = i + di[d];
+			int nj = j + dj[d];
+			
+			// 주어진 땅 크기를 넘어가거나 이미 확인을 마친 나라의 경우 넘어감
+			if (ni < 0 || nj < 0 || ni >= N || nj >= N || visited[ni][nj])
+				continue;
+			
+			// 인구 차이를 조사하여 국경선을 열지 말지 결정
+			if (Math.abs(A[i][j] - A[ni][nj]) >= L && Math.abs(A[ni][nj] - A[i][j]) <= R) {
+				visited[ni][nj] = true;
+				move.offer(new int[] {ni, nj});
+				dfs(ni, nj); sum+= A[ni][nj]; cnt++;
+			}
+		}
+	}
+	static boolean migration() {
 		/*
 		 * 상, 하, 좌, 우 인접한 나라와 인구수를 비교해 국경선이 열려있는 경우 나라 간 인구 이동을 시행하는 함수
 		 * 
-		 * Parameter
-		 * int[][] A: 각 나라의 인구수 표시
-		 * int L : 연결된 나라의 인구의 차이가 L이상인 경우
-		 * int R : 연결된 나라의 인구의 차이가 R이하인 경우 국경선 열기
-		 * 
 		 * Return
-		 * int : 이주를 하는 경우 1, 안하는 경우 0을 반환
+		 * int : 이주를 하는 경우 true, 안하는 경우 false을 반환
 		 */
+		boolean flag = false;
+		visited = new boolean[N][N]; // 그룹에 속해 인구 이동을 완료한 경우 1로 표시
+		move = new ArrayDeque<>();
 
-		int[][] visited = new int[A.length][A.length]; // 그룹에 속해 인구 이동을 완료한 경우 1로 표시
-
-		int[] di = { 0, 0, -1, 1 };
-		int[] dj = { 1, -1, 0, 0 };
-
-		int flag = 0; // return 값 정의
-
-		for (int i = 0; i < A.length; i++) {
-			label: for (int j = 0; j < A.length; j++) {
-				// 그룹의 총 인구수
-				int count = A[i][j];
-				// 그룹의 개수
-				int cnt = 1;
-				// 그룹을 찾는데 쓰는 queue
-				ArrayDeque<Node> q = new ArrayDeque<>();
-				// 해당 그룹을 이주하는데 사용하는 queue
-				ArrayDeque<Node> move = new ArrayDeque<>();
-
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
 				// visited가 1이면 이미 그룹에 속해 이동을 마친 나라이므로 넘어간다.
-				if (visited[i][j] == 1)
-					continue label;
-
-				// 아닌 경우 (i, j)에서부터 국경선이 열려 연결된 나라 그룹을 찾는다.
-				q.add(new Node(i, j));
-				move.add(new Node(i, j));
-				visited[i][j] = 1;
-
-				// 더 이상 그룹에 들어올 나라가 없는 경우 while문 종료
-				while (!q.isEmpty()) {
-					Node p = q.poll();
-
-					l: for (int d = 0; d < 4; d++) {
-						// 상, 하, 좌, 우 조사
-						int ni = p.x + di[d];
-						int nj = p.y + dj[d];
-
-						// 주어진 땅 크기를 넘어가거나 이미 이동을 마친 나라의 경우 넘어감
-						if (ni < 0 || nj < 0 || ni >= A.length || nj >= A.length || visited[ni][nj] == 1)
-							continue l;
-
-						// 인구 차이를 조사하여 국경선을 열지 말지 결정
-						if (Math.abs(A[p.x][p.y] - A[ni][nj]) >= L && Math.abs(A[ni][nj] - A[p.x][p.y]) <= R) {
-							visited[ni][nj] = 1;
-							count += A[ni][nj];
-							cnt++;
-
-							// 방문 표시를 하고, 다음 조사할 나라로 queue에 넣어줌
-							q.add(new Node(ni, nj));
-							move.add(new Node(ni, nj));
-						}
-					}
-				}
-				int m = count / cnt;
-				// 어떤 그룹에 나라가 2개 이상인 경우 국경선이 열려있다는 뜻이므로 flag 값 변경
-				if (cnt != 1)
-					flag = 1;
+				if (visited[i][j])
+					continue;
+				sum  = A[i][j]; cnt = 1;
+				dfs(i, j);
+				if(cnt != 1) flag = true;
 				while (!move.isEmpty()) {
-					Node np = move.poll();
-					A[np.x][np.y] = m;
-
+					int[] np = move.poll();
+					A[np[0]][np[1]] = sum/cnt;
+					
 				} // 인구 이동
 			}
-
 		}
 		return flag;
 	}
