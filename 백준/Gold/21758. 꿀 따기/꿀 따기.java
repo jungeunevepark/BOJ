a6b685942238903138207924ccdbe0102401f60d
evejj <101235186+jungeunevepark@users.noreply.github.com>
2023-02-23 12:02:16 +0900

    [Gold V] Title: 꿀 따기, Time: 208 ms, Memory: 14896 KB -BaekjoonHub

--- 백준/Gold/21758. 꿀 따기/꿀 따기.java
+++ 백준/Gold/21758. 꿀 따기/꿀 따기.java
@@ -3,18 +3,18 @@ import java.util.*;
 
 public class Main {
 	static int[] honey;
-	static int[] d;
+	static long[] d;
 	static boolean[] visited;
 	static int N, h;
 	static int[] comb;
-	static int max;
+	static long max;
 	public static void main(String[] args) throws Exception {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		N = Integer.parseInt(bf.readLine());
 		honey = new int[N];
 		visited = new boolean[N];
-		d = new int[N];
+		d = new long[N];
 		comb = new int[2];
 		StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
 		honey[0] = Integer.parseInt(st.nextToken());
@@ -28,7 +28,7 @@ public class Main {
 			// 양 끝
 			if(h != 0 && h != N-1) {
 				comb[0] = 0; comb[1] = N-1;
-				curc();
+				curc1();
 				// 계산
 			}
 			// 가장 왼쪽 + 왼쪽 부근
@@ -36,7 +36,7 @@ public class Main {
 				comb[0] = 0;
 				for(int k=1; k<h; k++) {
 					comb[1] = k;
-					curc();
+					curc3();
 				}
 			}
 			// 가장 오른쪽 + 오른쪽 부근
@@ -44,23 +44,22 @@ public class Main {
 				comb[1] = N-1;
 				for(int k = h+1; k<N-1; k++) {
 					comb[0] = k;
-					curc();
+					curc2();
 				}
 			}
 		}
 		System.out.println(max);
 	}
-	static void curc() {
-		int sum = 0;
-		if(comb[0] < h && h < comb[1]) {
-			sum = (d[h] - d[comb[0]]) + (d[comb[1]-1] - d[h] + honey[h]);
-		}
-		else if(comb[0] > h) {
-			sum = (d[comb[0]-1] - d[h] + honey[h]) + (d[comb[1]-1] - d[h] + honey[h] - honey[comb[0]]);		
-		}
-		else {
-			sum = (d[h] - d[comb[0]] - honey[comb[1]]) + (d[h] - d[comb[1]]);								
-		}
+	static void curc1() {
+		long sum = (d[h] - d[comb[0]]) + (d[comb[1]-1] - d[h] + honey[h]);
+		max = Math.max(max, sum);
+	}
+	static void curc2() {
+		long sum = (d[comb[0]-1] - d[h] + honey[h]) + (d[comb[1]-1] - d[h] + honey[h] - honey[comb[0]]);		
+		max = Math.max(max, sum);
+	}
+	static void curc3() {
+		long sum = (d[h] - d[comb[0]] - honey[comb[1]]) + (d[h] - d[comb[1]]);
 		max = Math.max(max, sum);
 	}
 }
